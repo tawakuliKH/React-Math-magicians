dde8b01c49a0ab4b54a0d91aa6a4331d59e63f8c
tawakuliKH <tawakuli456@gmail.com>
2023-10-21 15:40:16 +0330

    Update Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -8,39 +8,44 @@ const Calculator = () => {
   };
 
   return (
-    <div className="container">
-      <div className="result">
-        {result.next || result.total || 0}
+    <div className="conatainer-fluid">
+      <div>
+        <h2>Let&apos;s do some Calculations!</h2>
       </div>
-      <div className="numPad">
-        <div className="row">
-          <button type="button" className="btn" onClick={onClickHandler}>AC</button>
-          <button type="button" className="btn" onClick={onClickHandler}>+/-</button>
-          <button type="button" className="btn" onClick={onClickHandler}>%</button>
-          <button type="button" className="btn btn-orange" onClick={onClickHandler}>÷</button>
+      <div className="container">
+        <div className="result">
+          {result.next || result.total || 0}
         </div>
-        <div className="row">
-          <button type="button" className="btn" onClick={onClickHandler}>7</button>
-          <button type="button" className="btn" onClick={onClickHandler}>8</button>
-          <button type="button" className="btn" onClick={onClickHandler}>9</button>
-          <button type="button" className="btn btn-orange" onClick={onClickHandler}>x</button>
-        </div>
-        <div className="row">
-          <button type="button" className="btn" onClick={onClickHandler}>4</button>
-          <button type="button" className="btn" onClick={onClickHandler}>5</button>
-          <button type="button" className="btn" onClick={onClickHandler}>6</button>
-          <button type="button" className="btn btn-orange" onClick={onClickHandler}>-</button>
-        </div>
-        <div className="row">
-          <button type="button" className="btn" onClick={onClickHandler}>1</button>
-          <button type="button" className="btn" onClick={onClickHandler}>2</button>
-          <button type="button" className="btn" onClick={onClickHandler}>3</button>
-          <button type="button" className="btn btn-orange" onClick={onClickHandler}>+</button>
-        </div>
-        <div className="row">
-          <button type="button" className="btn btn-zero" onClick={onClickHandler}>0</button>
-          <button type="button" className="btn" onClick={onClickHandler}>.</button>
-          <button type="button" className="btn btn-orange" onClick={onClickHandler}>=</button>
+        <div className="numPad">
+          <div className="row">
+            <button type="button" className="btn" onClick={onClickHandler}>AC</button>
+            <button type="button" className="btn" onClick={onClickHandler}>+/-</button>
+            <button type="button" className="btn" onClick={onClickHandler}>%</button>
+            <button type="button" className="btn btn-orange" onClick={onClickHandler}>÷</button>
+          </div>
+          <div className="row">
+            <button type="button" className="btn" onClick={onClickHandler}>7</button>
+            <button type="button" className="btn" onClick={onClickHandler}>8</button>
+            <button type="button" className="btn" onClick={onClickHandler}>9</button>
+            <button type="button" className="btn btn-orange" onClick={onClickHandler}>x</button>
+          </div>
+          <div className="row">
+            <button type="button" className="btn" onClick={onClickHandler}>4</button>
+            <button type="button" className="btn" onClick={onClickHandler}>5</button>
+            <button type="button" className="btn" onClick={onClickHandler}>6</button>
+            <button type="button" className="btn btn-orange" onClick={onClickHandler}>-</button>
+          </div>
+          <div className="row">
+            <button type="button" className="btn" onClick={onClickHandler}>1</button>
+            <button type="button" className="btn" onClick={onClickHandler}>2</button>
+            <button type="button" className="btn" onClick={onClickHandler}>3</button>
+            <button type="button" className="btn btn-orange" onClick={onClickHandler}>+</button>
+          </div>
+          <div className="row">
+            <button type="button" className="btn btn-zero" onClick={onClickHandler}>0</button>
+            <button type="button" className="btn" onClick={onClickHandler}>.</button>
+            <button type="button" className="btn btn-orange" onClick={onClickHandler}>=</button>
+          </div>
         </div>
       </div>
     </div>
