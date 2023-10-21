95f38a82166bb58c4dca76de2b9cebc3bb371fc3
tawakuliKH <tawakuli456@gmail.com>
2023-10-21 16:20:47 +0330

    Delete navbar.js file

--- src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import { Link } from 'react-router-dom';
-
-const Navbar = () => (
-  <div className="navbar">
-    <Link to="/" className="nav-menu"> Quote</Link>
-    <Link to="/calculator" className="nav-menu"> Calculator</Link>
-  </div>
-);
-export default Navbar;
