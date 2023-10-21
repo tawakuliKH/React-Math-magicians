8de0a1310499fb31115b7b6f44ccf8f906c8c0f1
tawakuliKH <tawakuli456@gmail.com>
2023-10-21 16:18:38 +0330

    Update App.js for required changes by reviewer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Quote from './components/Quote';
 import Calculator from './components/Calculator';
 import Footer from './components/Footer';
@@ -8,14 +6,8 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Quote />} />
-          <Route path="/calculator" element={<Calculator />} />
-          <Route path="*" element={<h1>Page not found!</h1>} />
-        </Routes>
-      </Router>
+      <Quote />
+      <Calculator />
       <Footer />
     </div>
   );
