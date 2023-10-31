16ce4d729d352be212bed57a2eedbca26b6bc61a
tawakuliKH <tawakuli456@gmail.com>
2023-10-31 23:14:24 +0330

    Configure react routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Quote from './components/Quote';
 import Calculator from './components/Calculator';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Quote />
-      <Calculator />
-      <Footer />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
