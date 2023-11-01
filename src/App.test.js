4e7b82325ff016e087b4e00995ef0a7afb1afa27
tawakuliKH <tawakuli456@gmail.com>
2023-11-02 02:41:23 +0330

    Add another test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Magicians/i);
-
   expect(linkElement).toBeInTheDocument();
+});
+
+describe('App', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<App />);
+    expect(tree).toMatchSnapshot();
+  });
 });
